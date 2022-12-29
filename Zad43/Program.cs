996b6c4cbc2168b85ad9e12164f60c219f490a8b
Andrey <rf5800@mail.ru>
2022-12-29 10:50:54 +0300

    zad43

--- Zad43/Program.cs
+++ Zad43/Program.cs
@@ -4,3 +4,16 @@
 // y = k1*x+b1;
 // Пример:
 // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+Console.WriteLine("Задайте значение b1: ");
+double b1 = double.Parse(Console.ReadLine()!);
+Console.WriteLine("Задайте значение k1: ");
+double k1 = double.Parse(Console.ReadLine()!);
+Console.WriteLine("Задайте значение b2: ");
+double b2 = double.Parse(Console.ReadLine()!);
+Console.WriteLine("Задайте значение k2: ");
+double k2 = double.Parse(Console.ReadLine()!);
+double x = (b2 - b1) / (k1 - k2);
+double y = k1 * x + b1;
+Console.Write($"Точка пересечения двух прямых -> ({x}; {y})");
+
