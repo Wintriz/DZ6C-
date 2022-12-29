b096c5148e4e28f78db65b609aec17d6f932c179
Andrey <rf5800@mail.ru>
2022-12-29 10:27:06 +0300

    zad41

---
+++ Zad41/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// Пример:
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+Console.WriteLine("Введите пять чисел чезер пробел: ");
+int[] array = new int[5];
+string[] input = Console.ReadLine().Split(' ');
+int count =0;
+for (int i = 0; i < array.Length; ++i)
+{
+    array[i] = int.Parse(input[i]);
+    if (array[i] > 0)
+    {
+    count = count +1;
+    }
+
+}
+Console.Write("Количество чисел больше 0: ");
+Console.WriteLine(count);
